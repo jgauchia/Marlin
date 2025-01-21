5da7c0384c503e8fc2121dbe1be34c5698d29cce
jgauchia <jgauchia@gmx.es>
2025-01-22 00:02:17 +0100

    build: Set Y axis sensorless

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1216,7 +1216,7 @@
  * Override with M201
  *                                      X, Y, Z [, I [, J [, K...]]], E0 [, E1[, E2...]]
  */
-#define DEFAULT_MAX_ACCELERATION      { 500, 3000, 100, 10000 }
+#define DEFAULT_MAX_ACCELERATION      { 500, 500, 100, 10000 }
 
 //#define LIMITED_MAX_ACCEL_EDITING     // Limit edit via M201 or LCD to DEFAULT_MAX_ACCELERATION * 2
 #if ENABLED(LIMITED_MAX_ACCEL_EDITING)
@@ -1725,7 +1725,7 @@
 
 // The size of the printable area
 #define X_BED_SIZE 285
-#define Y_BED_SIZE 200
+#define Y_BED_SIZE 220
 
 // Travel limits (linear=mm, rotational=°) after homing, corresponding to endstop positions.
 #define X_MIN_POS 0
